aa7acfc382900176f53af86da6bfa54bf6d256c2
Tailine <tailinechagas@gmail.com>
2022-09-23 14:13:12 -0300

    feat: implements controller to sing in user

--- src/user/adapter/signin-user-controller.ts
+++ src/user/adapter/signin-user-controller.ts
@@ -1,10 +1,13 @@
-import jwt from 'jsonwebtoken'
 import { Request, Response } from 'express'
 import { Email } from '@shared/email'
 import { Password } from '@user/domain/password'
+import { AuthService } from '@user/application/service/auth-service'
+import { AppError } from '@shared/appError'
 
 export class SigninUserController {
-  static handle = async (request: Request, response: Response) => {
+  constructor(private authService: AuthService) {}
+
+  handle = async (request: Request, response: Response) => {
     const { email, password } = request.body
 
     const emailOrError = Email.create(email)
@@ -13,11 +16,20 @@ export class SigninUserController {
       return response.status(400).json({ message: 'Email ou senha inválidos' })
     }
 
-    // generate JWT token
-    // set token on cookie
+    const errorOrToken = await this.authService.signIn(email, password)
+    if (errorOrToken instanceof AppError) {
+      return response.status(errorOrToken.statusCode).json({
+        status: errorOrToken.statusCode,
+        message: errorOrToken.message
+      })
+    }
 
-    return response.status(200).json({ email, password })
-
-    // const token = jwt.sign()
+    return response
+      .cookie('access_token', errorOrToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production'
+      })
+      .status(200)
+      .json({ status: 200, message: 'Logged in successfully' })
   }
 }
