6464fcc6ab8328b6d75b7a1838c44147aeb233b1
Tailine <tailinechagas@gmail.com>
2022-10-15 18:43:58 -0300

    feat: create donation service

--- src/donation/application/services/create-donation-service.ts
+++ src/donation/application/services/create-donation-service.ts
@@ -1,10 +1,57 @@
-import { Donation } from '@donation/domain/donation'
-import { CreateDonationUseCase } from '../port/create-donation-use-case'
-
-interface
+import { CategoryRepository } from '@category/adapter/persistence/category-repository'
+import {
+  DonationDbData,
+  DonationRepository
+} from '@donation/adapter/persistence/donation-repository-port'
+import { ImageStorage } from '@external-libraries/supabase-storage/image-storage'
+import { AppError } from '@shared/appError'
+import { UserRepository } from '@user/adapter/persistence/user-repository-port'
+import { CreateDonationUseCase } from '../port/create-donation-port'
+import { DonationData } from '../port/donation-data'
 
 export class CreateDonationService implements CreateDonationUseCase {
-  execute(donation: Donation, userId: string): Promise<boolean> {
-    return Promise.resolve(true)
+  constructor(
+    private userRepository: UserRepository,
+    private donationRepository: DonationRepository,
+    private categoryRepository: CategoryRepository,
+    private imageStorage: ImageStorage
+  ) {}
+
+  async execute(
+    donation: DonationData,
+    userId: string
+  ): Promise<DonationDbData | AppError> {
+    if (!(await this.userRepository.findById(userId))) {
+      return new AppError('Usuário não existe.')
+    }
+
+    if (!(await this.categoryRepository.findById(donation.categoryId))) {
+      return new AppError('Categoria não existe.')
+    }
+
+    const uploadedResult = await this.imageStorage.upload(
+      userId,
+      donation.images
+    )
+
+    const hasUploadFailed = uploadedResult.every((result) => result.error)
+    if (hasUploadFailed) {
+      return new AppError('Erro ao salvar imagens. Tente novamente mais tarde')
+    }
+
+    const uploadedImages = []
+    for (const result of uploadedResult) {
+      if (result.data) {
+        const imageUrl = this.imageStorage.getImageUrl(result.data.Key)
+        if (imageUrl) {
+          uploadedImages.push(result.data.Key)
+        }
+      }
+    }
+
+    return this.donationRepository.createDonation({
+      ...donation,
+      images: uploadedImages
+    })
   }
 }
