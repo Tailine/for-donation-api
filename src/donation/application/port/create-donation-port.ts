4f0fbcbf1e5e965f84cc495ff30923a896fd62a9
Tailine <tailinechagas@gmail.com>
2022-10-17 16:43:20 -0300

    chore: update return type for donation created

--- src/donation/application/port/create-donation-port.ts
+++ src/donation/application/port/create-donation-port.ts
@@ -1,10 +1,7 @@
-import { DonationDbData } from '@donation/adapter/persistence/donation-repository-port'
+import { Donation } from '@donation/adapter/persistence/donation.entity'
 import { AppError } from '@shared/appError'
 import { DonationData } from './donation-data'
 
 export interface CreateDonationUseCase {
-  execute(
-    donation: DonationData,
-    userId: string
-  ): Promise<DonationDbData | AppError>
+  execute(donation: DonationData, userId: string): Promise<Donation | AppError>
 }
