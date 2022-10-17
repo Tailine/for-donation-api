0c375c4f854bd8bf7e6f164be0d0d2b86bfd1950
Tailine <tailinechagas@gmail.com>
2022-10-17 17:01:49 -0300

    feat: implements repository methods
    - creates donation
    - return donations

--- src/donation/adapter/persistence/typeorm-donation-repository-adapter.ts
+++ src/donation/adapter/persistence/typeorm-donation-repository-adapter.ts
@@ -1,6 +1,8 @@
+import { Category } from '@category/adapter/persistence/category.entity'
 import { postgresDataSource } from '@config/typeorm'
+import { User } from '@user/adapter/persistence/user.entity'
 import { Repository } from 'typeorm'
-import { DonationDbData, DonationRepository } from './donation-repository-port'
+import { NewDonation, DonationRepository } from './donation-repository-port'
 import { Donation } from './donation.entity'
 
 export class TypeormDonationRepository implements DonationRepository {
@@ -10,7 +12,48 @@ export class TypeormDonationRepository implements DonationRepository {
     this.donationRepository = postgresDataSource.getRepository(Donation)
   }
 
-  async createDonation(donationData: DonationDbData): Promise<DonationDbData> {
-    return this.donationRepository.save(donationData)
+  async createDonation(
+    donationData: NewDonation,
+    userId: string
+  ): Promise<Donation> {
+    const { categoryId } = donationData
+    const donation = TypeormDonationRepository.createDonationEntity(
+      donationData,
+      userId,
+      categoryId
+    )
+    const newDonation = await this.donationRepository.save(donation)
+    return newDonation
+  }
+
+  async findAll(): Promise<Donation[]> {
+    return await this.donationRepository.find({
+      relations: {
+        category: true
+      }
+    })
+  }
+
+  static createDonationEntity(
+    donationData: NewDonation,
+    userId: string,
+    categoryId: number
+  ): Donation {
+    const category = new Category()
+    category.id = categoryId
+
+    const user = new User()
+    user.id = userId
+
+    const donation = new Donation()
+    donation.category = category
+    donation.user = user
+    donation.images = donationData.images
+    donation.email = donationData.email
+    donation.description = donationData.description
+    donation.phone = donationData.phone
+    donation.title = donationData.title
+
+    return donation
   }
 }
