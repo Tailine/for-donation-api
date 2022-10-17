150e34231e3db124350603a8846e71a722a67c40
Tailine <tailinechagas@gmail.com>
2022-10-17 16:55:59 -0300

    chore: update DonationRepository methods
    - change createDonation parameter types
    - adds findAll method to get all donations

--- src/donation/adapter/persistence/donation-repository-port.ts
+++ src/donation/adapter/persistence/donation-repository-port.ts
@@ -1,12 +1,15 @@
 import { DonationData } from '@donation/application/port/donation-data'
 import { AppError } from '@shared/appError'
+import { Donation } from './donation.entity'
 
 export interface DonationRepository {
   createDonation(
-    donationData: DonationDbData
-  ): Promise<DonationDbData | AppError>
+    donationData: NewDonation,
+    userId: string
+  ): Promise<Donation | AppError>
+  findAll(): Promise<Donation[]>
 }
 
-export type DonationDbData = Omit<DonationData, 'images'> & {
+export type NewDonation = Omit<DonationData, 'images'> & {
   images: string[]
 }
