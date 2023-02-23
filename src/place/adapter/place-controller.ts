2ebc8c1cd4a97a46cc82535dbb06b13e9454bb0d
Tailine <tailinechagas@gmail.com>
2023-02-23 18:36:46 -0300

    feat: change response format

--- src/place/adapter/place-controller.ts
+++ src/place/adapter/place-controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
 import { PlaceQuery } from './../application/ports/place-query'
+
 export class PlaceController {
   constructor(private placeQueryService: PlaceQuery) {}
 
   handleState = async (_: Request, response: Response) => {
     try {
       const states = await this.placeQueryService.getStates()
-      return response.status(200).json({ data: states })
+      return response.status(200).json(states)
     } catch (err) {
       console.log({ err })
       return response.status(500).json({
@@ -26,7 +27,7 @@ export class PlaceController {
       }
 
       const cities = await this.placeQueryService.getCities(stateId)
-      return response.status(200).json({ data: cities })
+      return response.status(200).json(cities)
     } catch (err) {
       return response.status(500).json({
         message: 'Erro no servidor, tente novamente mais tarde'
