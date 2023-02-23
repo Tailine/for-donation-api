9386de42e20e9f3ae4eee69de1a0e0e59b110a7d
Tailine <tailinechagas@gmail.com>
2023-02-23 18:19:40 -0300

    feat: add cors and category routes

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,23 @@ import '@config/typeorm'
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import { donationRoutes } from 'routes/donation'
+import { categoryRoutes } from 'routes/category'
 import { userRoutes } from 'routes/user'
 import { placeRoutes } from 'routes/place'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors({ credentials: true, origin: process.env.ORIGIN }))
 app.use(cookieParser())
 app.use(express.json())
 
-app.use('/donation', donationRoutes)
-app.use('/user', userRoutes)
-app.use('/place', placeRoutes)
+app.use('/donations', donationRoutes)
+app.use('/users', userRoutes)
+app.use('/places', placeRoutes)
+app.use('/categories', categoryRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
